509bbec81339a888bdc40861551cc6cfcedc61cb
David Pätzel <david.paetzel@posteo.de>
2023-06-07 14:40:17 +0200

    ✨ Add seed support to `submit.py optparams`

--- scripts/many-algorithms/submit.py
+++ scripts/many-algorithms/submit.py
@@ -25,23 +25,25 @@ import slurm
 defaults = dict(n_iter=100000, timeout=10)
 
 
-def forall_npzs(path, code):
+def forall_npzs(path, code, seed=None):
     """
     Parameters
     ----------
     path : str
         Path to an NPZ file or a directory where NPZ files reside in.
     code : callable
-        Will be called as `code(path)` if `path` is a file and `code(fpath)` for
+        Will be called as `code(path, seed)` if `path` is a file and `code(fpath)` for
         each NPZ file with path `fpath` under `path` if `path` is a directory.
+    seed : int or None
+        Seed to be used for initializing the algorithms' RNGs.
     """
     if os.path.isfile(path):
-        code(path)
+        code(path, seed)
     elif os.path.isdir(path):
         for fname in os.listdir(path):
             fpath = os.path.join(path, fname)
             if fname.endswith(".npz") and os.path.isfile(fpath):
-                code(fpath)
+                code(fpath, seed)
                 time.sleep(0.5)
 
 
@@ -69,6 +71,13 @@ def cli(ctx):
     show_default=True,
     help="Compute budget (in seconds) for the hyperparameter optimization",
 )
+@click.option(
+    "--seed",
+    type=click.IntRange(min=0),
+    show_default=True,
+    default=None,
+    help="Seed for initializing RNGs",
+)
 @click.option("--experiment-name", type=str, default="optparams")
 @click.option("--node", type=str, default="oc-compute03")
 @click.option(
@@ -77,11 +86,11 @@ def cli(ctx):
     type=str,
     default=None,
     show_default=True,
-    help=("Override Slurm options " "(for now, see file source for defaults)"),
+    help=("Override Slurm options (for now, see file source for defaults)"),
 )
 @click.argument("PATH")
 @click.pass_context
-def optparams(ctx, timeout, experiment_name, node, slurm_options, path):
+def optparams(ctx, timeout, seed, experiment_name, node, slurm_options, path):
     """
     If PATH is an NPZ file, then submit a single job for that file. If PATH is a
     directory, look at its immediate contents (i.e. non-recursively) and submit
@@ -98,14 +107,15 @@ def optparams(ctx, timeout, experiment_name, node, slurm_options, path):
     mlflow.set_tracking_uri(tracking_uri)
     mlflow.create_experiment(experiment_name)
 
-    def submit_npz(npzfile):
+    def submit_npz(npzfile, seed):
         command = (
             # Note that we keep `{job_dir}` to be inserted by `submit`.
             f'python {{dir_job}}/scripts/many-algorithms/run.py "{npzfile}" optparams '
             f"--tracking-uri={tracking_uri} "
             f"--experiment-name={experiment_name} "
             "--run-name=${{SLURM_ARRAY_JOB_ID}}_${{SLURM_ARRAY_TASK_ID}} "
-            f"--timeout={timeout}"
+            f"--timeout={timeout} "
+            f"{'' if seed is None else f'--seed={seed}'} "
         )
         slurm.submit(
             command,
@@ -121,7 +131,7 @@ def optparams(ctx, timeout, experiment_name, node, slurm_options, path):
             dir_results=dir_results,
         )
 
-    forall_npzs(path, submit_npz)
+    forall_npzs(path, submit_npz, seed=seed)
 
 
 if __name__ == "__main__":
