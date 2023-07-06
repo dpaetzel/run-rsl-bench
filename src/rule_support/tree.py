ac5589025e24f2b02ac602ba41588587020211c9
David Pätzel <david.paetzel@posteo.de>
2023-07-06 14:31:18 +0200

    🎨 Extract rule extract from DecisionTreeRegressor

--- src/rule_support/tree.py
+++ src/rule_support/tree.py
@@ -21,87 +21,93 @@ from sklearn.utils.validation import check_is_fitted  # type: ignore
 
 
 class DecisionTreeRegressor(sklearn.tree.DecisionTreeRegressor):
-    # TODO Consider to make this general (i.e. include classification as well)
     @property
     def rules_(self):
 
         check_is_fitted(self)
 
-        if hasattr(self, "feature_names_in_"):
-            feature_names = self.feature_names_in_
+        return extract_rules(self)
+
+
+# TODO Consider to make this/check whether it already is general (i.e. include
+# classification as well)
+def extract_rules(estimator):
+    if hasattr(estimator, "feature_names_in_"):
+        feature_names = estimator.feature_names_in_
+    else:
+        feature_names = ["feature_" + str(i) for i in range(estimator.n_features_)]
+
+    rules = []
+
+    leq_threshold = "leq_threshold"
+    gt_threshold = "gt_threshold"
+
+    def is_leaf(node):
+        # TODO Maybe check children_right and children_left for
+        # being -1 as well for consistency
+        return estimator.tree_.feature[node] == _tree.TREE_UNDEFINED
+
+    def recurse(node, features, rels, thresholds, feature_names):
+        if not is_leaf(node):
+            features_ = features + [feature_names[estimator.tree_.feature[node]]]
+            thresholds_ = thresholds + [estimator.tree_.threshold[node]]
+
+            recurse(
+                estimator.tree_.children_left[node],
+                features_,
+                # If we go down the left subtree, then the data is less
+                # than or equal to the threshold.
+                rels + [leq_threshold],
+                thresholds_,
+                feature_names,
+            )
+            recurse(
+                estimator.tree_.children_right[node],
+                features_,
+                # If we go down the left subtree, then the data is
+                # greater than the threshold.
+                rels + [gt_threshold],
+                thresholds_,
+                feature_names,
+            )
         else:
-            feature_names = ["feature_" + str(i) for i in range(self.n_features_)]
+            lowers, uppers = [], []
+            features = np.array(features)
+            # Says whether data <= threshold or data > threshold.
+            rels = np.array(rels)
+            thresholds = np.array(thresholds)
 
-        rules = []
+            for feature_name in feature_names:
 
-        leq_threshold = "leq_threshold"
-        gt_threshold = "gt_threshold"
+                # Lower bound (i.e. `data > threshold`).
+                #
+                # If no threshold below the data, set lower bound to
+                # negative infinity.
+                if not np.any(rels == gt_threshold):
+                    lowers.append(-np.inf)
+                else:
+                    # Otherwise, lower bound is highest of the lower thresholds.
+                    lowers.append(np.max(thresholds[rels == gt_threshold]))
 
-        def is_leaf(node):
-            # TODO Maybe check children_right and children_left for
-            # being -1 as well for consistency
-            return self.tree_.feature[node] == _tree.TREE_UNDEFINED
+                # Upper bound (i.e. `data <= threshold`).
+                #
+                # If no threshold above the data, set to upper bound to
+                # positive infinity.
+                if not np.any(rels == leq_threshold):
+                    uppers.append(np.inf)
+                else:
+                    # Otherwise, upper bound is lowest of the upper thresholds.
+                    uppers.append(np.min(thresholds[rels == leq_threshold]))
 
-        def recurse(node, features, rels, thresholds, feature_names):
-            if not is_leaf(node):
-                features_ = features + [feature_names[self.tree_.feature[node]]]
-                thresholds_ = thresholds + [self.tree_.threshold[node]]
-
-                recurse(
-                    self.tree_.children_left[node],
-                    features_,
-                    # If we go down the left subtree, then the data is less
-                    # than or equal to the threshold.
-                    rels + [leq_threshold],
-                    thresholds_,
-                    feature_names,
+            rules.append(
+                dict(
+                    l=np.array(lowers),
+                    u=np.array(uppers),
+                    pred=estimator.tree_.value[node],
+                    exp=estimator.tree_.n_node_samples[node],
                 )
-                recurse(
-                    self.tree_.children_right[node],
-                    features_,
-                    # If we go down the left subtree, then the data is
-                    # greater than the threshold.
-                    rels + [gt_threshold],
-                    thresholds_,
-                    feature_names,
-                )
-            else:
-                lowers, uppers = [], []
-                features = np.array(features)
-                # Says whether data <= threshold or data > threshold.
-                rels = np.array(rels)
-                thresholds = np.array(thresholds)
+            )
 
-                for feature_name in feature_names:
+    recurse(0, [], [], [], feature_names)
 
-                    # Lower bound (i.e. `data > threshold`).
-                    #
-                    # If no threshold below the data, set lower bound to
-                    # negative infinity.
-                    if not np.any(rels == gt_threshold):
-                        lowers.append(-np.inf)
-                    else:
-                        # Otherwise, lower bound is highest of the lower thresholds.
-                        lowers.append(np.max(thresholds[rels == gt_threshold]))
-
-                    # Upper bound (i.e. `data <= threshold`).
-                    #
-                    # If no threshold above the data, set to upper bound to
-                    # positive infinity.
-                    if not np.any(rels == leq_threshold):
-                        uppers.append(np.inf)
-                    else:
-                        # Otherwise, upper bound is lowest of the upper thresholds.
-                        uppers.append(np.min(thresholds[rels == leq_threshold]))
-
-                rules.append(
-                    dict(
-                        l=np.array(lowers),
-                        u=np.array(uppers),
-                        pred=self.tree_.value[node],
-                    )
-                )
-
-        recurse(0, [], [], [], feature_names)
-
-        return rules
+    return rules
